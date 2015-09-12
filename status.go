7af1be22815288670f6f94d24da4afec239edbc8
Tim Retout <tim@retout.co.uk>
2015-09-12 22:50:57 +0100

    Add extra status codes from RFC 3977

--- status.go
+++ status.go
@@ -19,4 +19,48 @@ package nntp
 const (
 	StatusHelpText       = 100
 	StatusCapabilityList = 101
+	StatusServerDate     = 111
+
+	StatusPostingAllowed       = 200
+	StatusPostingProhibited    = 201
+	StatusConnectionClosing    = 205
+	StatusGroupSelected        = 211
+	StatusInformationFollows   = 215
+	StatusArticleFollows       = 220
+	StatusArticleHeadersFollow = 221
+	StatusArticleBodyFollows   = 222
+	StatusArticleSelected      = 223
+	StatusOverviewFollows      = 224
+	StatusHeadersFollow        = 225
+	StatusNewArticlesFollow    = 230
+	StatusNewNewsgroupsFollow  = 231
+	StatusArticleTransferredOK = 235
+	StatusArticleReceivedOK    = 240
+
+	StatusTransferArticle = 335
+	StatusPostArticle     = 340
+
+	StatusServiceNotAvailable   = 400
+	StatusWrongServerMode       = 401
+	StatusInternalFault         = 403
+	StatusNoSuchGroup           = 411
+	StatusNoGroupSelected       = 412
+	StatusCurrentArticleInvalid = 420
+	StatusNoNextArticle         = 421
+	StatusNoPreviousArticle     = 422
+	StatusNoSuchArticleNumber   = 423
+	StatusNoSuchMessageID       = 430
+	StatusArticleNotWanted      = 435
+	StatusTransferFailed        = 436
+	StatusTransferRejected      = 437
+	StatusPostingNotPermitted   = 440
+	StatusPostingFailed         = 441
+	StatusNotAuthenticated      = 480
+	StatusNotPrivate            = 483
+
+	StatusUnknownCommand      = 500
+	StatusSyntaxError         = 501
+	StatusNotPermitted        = 502
+	StatusFeatureNotSupported = 503
+	StatusEncodingError       = 504
 )
