0ef8b0c7773b3101bd91fd54b3b86230880bdebc
Tim Retout <tim@retout.co.uk>
2015-09-13 23:06:41 +0100

    Describe server.go

--- server.go
+++ server.go
@@ -13,6 +13,8 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+// NNTP server.  See RFC 3977.
+
 package nntp
 
 type Handler interface {
